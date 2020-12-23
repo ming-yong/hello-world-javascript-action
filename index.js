67d55431d2c5d6196c3234247c423f504e163585
mingyong.dev <mingyong.dev@gmail.com>
2020-12-23 16:35:08 -0500

    modify version

--- index.js
+++ index.js
@@ -1,42 +1,34 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
-
-// try {
-//   // `who-to-greet` input defined in action metadata file
-//   const nameToGreet = core.getInput('who-to-greet');
-//   console.log(`Hello ${nameToGreet}!`);
-//   const time = (new Date()).toTimeString();
-//   core.setOutput("time", time);
-//   // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   console.log(`The event payload: ${payload}`);
-// } catch (error) {
-//   core.setFailed(error.message);
-// }
-
-//From here
-
 const { Toolkit } = require("actions-toolkit");
-const dotenv = require("dotenv");
-dotenv.config();
 const axios = require("axios").default;
 const btoa = require("btoa");
+const dotenv = require("dotenv");
+dotenv.config();
 
 try {
 	Toolkit.run(async (tools) => {
-		// Assign owner and repo data to variables
-
 		const owner = process.env.REPO_OWNER;
-    const repo = process.env.REPO;
+		const repo = process.env.REPO;
+		const target_branch = "dev-posts";
+		const today = new Date();
+		const pr_title = `Dev posts ${today.getFullYear()}/${today.getMonth() + 1}`;
+		// For my latest post's date
+		let myPostDate;
+		let myPosts;
+		let path = "_posts";
+		// For my latest DEV posts
+		const checkPostLimit = 15;
+		let newJekyllPostFileName;
+		let checkPostAmount;
+		let devPostContent;
+		let devPostTitle;
+		let devPostDate;
+		let devPostURL;
+		let devPosts;
 
-		// Get the latest post in my jekyll site
-		var path = "_posts";
-		var myPosts; // All posts in repo
-		var myPostDate; // Latest repo post date
-		var refsData; // Data on Current Repo Refs
-		var newJekyllPostFileName;
-
-		// Get repo posts data
+		/**
+		 * Get my latest post's date from the repo posts data
+		 */
 		myPosts = (
 			await tools.github.repos.getContent({
 				owner,
@@ -45,19 +37,12 @@ try {
 			})
 		).data;
 
-    myPostDate = myPosts[0]["name"].slice(0, 10);
-    
-		// Get the latest DEV posts
-		var devPosts;
-		var devPostDate;
-		var devPostTitle;
-		var devPostURL;
-		var masterRepoSHA; // SHA of Master Branch in Repo
-		var devPostContent;
-		var checkPostAmount;
+		myPostDate = myPosts[0]["name"].slice(0, 10);
 
-		// Create headers for DEV request
-		var headers = {
+		/**
+		 * Fetch DEV posts
+		 */
+		let headers = {
 			"Content-Type": "application/json",
 			"api-key": `${process.env.DEV_API_KEY}`,
 		};
@@ -73,7 +58,7 @@ try {
 
 		// Assign DEV data
 		devPosts = (await getData()).data;
-    checkPostAmount = devPosts.length > 10 ? 10 : devPosts.length;
+		checkPostAmount = devPosts.length > checkPostLimit ? checkPostLimit : devPosts.length;
 
 		for (let index = 0; index < checkPostAmount; index++) {
 			devPostDate = devPosts[index]["published_at"]; // ex. 2020-02-12T12:45:27.741Z
@@ -96,86 +81,39 @@ try {
         `.trim();
 
 				// Remove extraneous indentation
-        fileContents = fileContents.replace(/^ {4}/gm, "");
-        
+				fileContents = fileContents.replace(/^ {4}/gm, "");
+
 				// Encode it in Base64 Encoding
 				const encodedContents = btoa(fileContents);
 
-				// Get list of repo branches
-				refsData = (
-					await tools.github.git.listRefs({
-						owner,
-						repo,
-					})
-				).data;
-
-				// If branch does not exist, create branch
-				if (refsData.filter((data) => data.ref == "refs/heads/dev_to_jekyll").length == 0) {
-					// Get Master Branch SHA
-					refsFiltered = refsdata.filter((ref) => ref.ref == "refs/heads/master");
-					masterRepoSHA = refsFiltered[0]["object"]["sha"];
-
-					// Create a New Branch for the PR
-					newBranch = await tools.github.git.createRef({
-						owner,
-						repo,
-						ref: "refs/heads/dev_to_jekyll",
-						sha: masterRepoSHA,
-					});
-
-					// Create a new file in the new branch
-					newFile = await tools.github.repos.createOrUpdateFile({
-						owner,
-						repo,
-						branch: "dev_to_jekyll",
-						path: `_posts/${newJekyllPostFileName}`,
-						message: `New markdown file for ${devPostTitle}`,
-						content: encodedContents,
-					});
-				}
-
-				// Create Pull Request
-				// Get list of all pull requests in working branch
-				var prArray = (
-					await tools.github.pulls.list({
-						owner,
-						repo,
-						head: "dev_to_jekyll",
-					})
-				).data;
-
-				var prArrayFiltered = prArray.filter((pr) => pr.title == `New DEV Post: ${devPostTitle}`);
-
-				// If PR exists, update current pull request
-				if (prArrayFiltered.length > 0) {
-					var prNumber = prArrayFiltered[0].number;
-					newPr = await tools.github.pulls.update({
-						owner,
-						repo,
-						pull_number: prNumber,
-					});
-					tools.log.success("PR updated");
-
-					// If PR does not exist, create a new one
-				} else if (prArrayFiltered.length == 0) {
-					newPR = await tools.github.pulls.create({
-						owner,
-						repo,
-						title: `New DEV Post: ${devPostTitle}`,
-						head: "dev_to_jekyll",
-						base: "master",
-						body: `Automated PR to add the new DEV blog post, ${devPostTitle}, to your Jekyll site as markdown.`,
-					});
-					tools.log.success("PR created");
-				}
-				tools.exit.success("Processing complete");
+				// Create that file in our branch
+				tools.github.repos.createOrUpdateFileContents({
+					owner,
+					repo,
+					branch: target_branch,
+					path: `_posts/${newJekyllPostFileName}`,
+					message: `New markdown file for ${devPostTitle}`,
+					content: encodedContents,
+				});
 			}
+
+			tools.log.success(`Post#${ index }: Added ${ devPostTitle }!`);
 		}
-		tools.exit.success("There are no posts on DEV newer than the posts on your Jekyll site.");
+
+		/**
+		 * Create a PR
+		 */
+		tools.github.pulls.create({
+			owner,
+			repo,
+			title: pr_title,
+			head: "master",
+			base: target_branch,
+		});
+
+		tools.exit.success("Done!");
 	});
 
-	const payload = JSON.stringify(github.context.payload, undefined, 2);
-	console.log(`The event payload: ${payload}`);
 } catch (error) {
 	core.setFailed(error.message);
 }
